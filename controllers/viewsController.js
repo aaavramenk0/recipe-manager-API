41bd7acd9560af5e8470d153bc8f4c4fb45c3d1c
waheednazeer <waheed.nazeer@hotmail.com>
2023-07-11 14:58:35 +0500

    recipe-detail route controller working

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,20 +1,21 @@
 const db = require('../models');
 const Recipe = db.recipe;
 
-const viewsController= {};
+//const viewsController= {};
 
-viewsController.buildRecipeView = async function(req, res){
+const buildRecipeView = async function(req, res){
     let card;
   
     //req.flash("notice", "This is a flash message.")
 
     Recipe.find({})
     .then((data) => {
-      //console.log(data);
+      //console.log(data[0]._id);
       card = `<ul class="recipeCard">`
       data.forEach(element => {
+        //console.log("inside "+element._id);
         card += '<li>'
-        card += `<img src= "`+ element.imgUrl +`">`
+        card += `<a href="` + `/views/recipe-detail/`+ element._id +`"><img src= "`+ element.imgUrl +`">` + `</a>`
         card += `<h2>`+ element.name + `</h2>`
         card += `</li>`
        
@@ -26,12 +27,35 @@ viewsController.buildRecipeView = async function(req, res){
         card,
       })
     
-    })
-        
-   
-  
-   
-    
+    })  
   }
 
-  module.exports = viewsController;
+/********************************
+* build recipe detail view
+* 
+********************************/
+const buildRecipeDetailView = async function(req, res){
+  try {
+    const _id = req.params.id; 
+    Recipe.findById(_id)
+      .then((data) => {
+        if (!data) {
+          return res.status(404).send({ message: 'Recipe not found.' });
+        }
+        res.send(data);
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: err.message || 'Error occurred while retrieving the recipe.',
+        });
+      });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+
+
+
+
+  module.exports = {buildRecipeView, buildRecipeDetailView};
--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -2,7 +2,10 @@ const viewsController = require('../controllers/viewsController');
 
 const router = require('express').Router();
 
-router.get('/', viewsController.buildRecipeView());
+router.get('/', viewsController.buildRecipeView);
+
+router.get('/recipe-detail/:id', viewsController.buildRecipeDetailView);
+
 
 
 module.exports = router;
