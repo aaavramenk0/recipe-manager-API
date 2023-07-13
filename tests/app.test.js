45db597038429e8dff4443eb8901404dd42f0cac
Aleksandr <alex.avramenko2004@gmail.com>
2023-07-13 16:29:46 -0400

    Tested DELETE routes and GET by id routes

--- tests/app.test.js
+++ tests/app.test.js
@@ -29,65 +29,76 @@ afterEach(async () => {
     await mongoose.connection.close();
 });
 
-// GET test for the /views page
-describe('GET /views', () => {
-    test('should return recipes view', async () => {
-        const res = await supertest(app).get("/views");
-        expect(res.statusCode).toBe(201);
-    });
-});
-
-// GET test for /recipes route
-describe('GET /recipes', () => {
+// GET test for all get routes
+describe('GET requests', () => {
     test('should return all recipes from the database', async () => {
         await api
             .get('/recipes')
             .expect(200)
             .expect('Content-Type', /application\/json/);
     });
-});
-
-// GET test for /authors route
-describe('GET /authors', () => {
+    
     test('should return all authors from the database', async () => {
         await api
             .get('/authors')
             .expect(200)
             .expect('Content-Type', /application\/json/);
     });
+
+    test('should return recipes view', async () => {
+        const res = await supertest(app).get("/views");
+        expect(res.statusCode).toBe(201);
+    });
+});
+
+// GET test for /recipes/id route
+describe('GET /recipes/id', () => {
+    test('should GET recipe by id', async () => {
+        // get all items
+        const recipes = await Recipe.find({});
+        // get the first item parsed to JSON
+        const firstRecipe = recipes[0].toJSON();
+        // get the result expecting success and JSON data
+        const resRecipe = await
+            api.get(`/recipes/${firstRecipe._id}`)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+        // check if the item has the same id and the route works as expected
+        expect(resRecipe.body.id).toEqual(firstRecipe.id)
+    })
 });
 
 
-// POST test for /recipes route
-describe('POST /recipes', () => {
+// POST tests for all posts routes
+describe('POST requests', () => {
     test('should put a new recipe to the database', async () => {
         // build a new recipe 
         const newRecipe = {
-            "imgUrl" : "https://www.budgetbytes.com/wp-content/uploads/2023/06/BLT-Pasta-Salad-side.jpg",
-            "name" : "BLT PASTA SALAD",
+            "imgUrl": "https://www.budgetbytes.com/wp-content/uploads/2023/06/BLT-Pasta-Salad-side.jpg",
+            "name": "BLT PASTA SALAD",
             "rating": "4.75 from 4 votes",
-            "description" : "This BLT pasta salad recipe is the perfect summer meal! It's easy to make, full of flavor, and perfect for a potluck or picnic.",
-            "author" : {
-                "name" : "Beth",
-                "url" : "https://www.budgetbytes.com/author/beth/"
+            "description": "This BLT pasta salad recipe is the perfect summer meal! It's easy to make, full of flavor, and perfect for a potluck or picnic.",
+            "author": {
+                "name": "Beth",
+                "url": "https://www.budgetbytes.com/author/beth/"
             },
-            "cookTime" : "25minutes mins",
-            "ingredients" : ["8 oz. bacon", "3/4 cup mayonnaise", "1/4 cup ranch dressing", "2 Tbsp lemon juice", "1/2 tsp salt", "1/4 tsp freshly cracked pepper", "12 oz. bowtie pasta", "1 pint grape tomatoes", "1/4 red onion", "2 cups fresh spinach (packed)"],
-            "instructions" : [
-                "Cook the bacon in a large skillet over medium heat until brown and crispy. Transfer the cooked bacon to a paper towel-lined plate to drain and cool. Once cooled, chop or crumble the bacon into smaller pieces.", 
-                "While the bacon is cooking, prepare the dressing. Stir together the mayonnaise, ranch dressing, lemon juice, salt, and pepper. Set the dressing aside.", 
-                "Bring a large pot of salted water to a boil for the pasta. Once boiling, add the pasta and cook until tender (7-8 minutes). Drain the pasta in a colander, rinse it briefly with cool water to cool it down, then let it drain very well.", 
-                "While the pasta is cooking and draining, prepare the vegetables. Slice the grape tomatoes in half, finely dice the red onion, and roughly chop the spinach.", 
-                "Once the pasta is cooled and drained, add it to a large bowl along with the spinach, tomatoes, onions, and dressing. Stir everything together until evenly combined and coated in dressing. Let the pasta sit for 10-15 minutes for the flavors to blend, then stir once more and serve." 
+            "cookTime": "25minutes mins",
+            "ingredients": ["8 oz. bacon", "3/4 cup mayonnaise", "1/4 cup ranch dressing", "2 Tbsp lemon juice", "1/2 tsp salt", "1/4 tsp freshly cracked pepper", "12 oz. bowtie pasta", "1 pint grape tomatoes", "1/4 red onion", "2 cups fresh spinach (packed)"],
+            "instructions": [
+                "Cook the bacon in a large skillet over medium heat until brown and crispy. Transfer the cooked bacon to a paper towel-lined plate to drain and cool. Once cooled, chop or crumble the bacon into smaller pieces.",
+                "While the bacon is cooking, prepare the dressing. Stir together the mayonnaise, ranch dressing, lemon juice, salt, and pepper. Set the dressing aside.",
+                "Bring a large pot of salted water to a boil for the pasta. Once boiling, add the pasta and cook until tender (7-8 minutes). Drain the pasta in a colander, rinse it briefly with cool water to cool it down, then let it drain very well.",
+                "While the pasta is cooking and draining, prepare the vegetables. Slice the grape tomatoes in half, finely dice the red onion, and roughly chop the spinach.",
+                "Once the pasta is cooled and drained, add it to a large bowl along with the spinach, tomatoes, onions, and dressing. Stir everything together until evenly combined and coated in dressing. Let the pasta sit for 10-15 minutes for the flavors to blend, then stir once more and serve."
             ],
-            "equipment" : ["Chef's knife", "White cutting boards", "Nonstick fry pans"],
-            "nutrition" : {
-                "protein" : "10g",
-                "fiber" : "2g",
-                "calories" : "466kcal",
-                "fat" : "31g",
-                "carbohydrates" : "36g",
-                "sodium" : "546mg"
+            "equipment": ["Chef's knife", "White cutting boards", "Nonstick fry pans"],
+            "nutrition": {
+                "protein": "10g",
+                "fiber": "2g",
+                "calories": "466kcal",
+                "fat": "31g",
+                "carbohydrates": "36g",
+                "sodium": "546mg"
             }
         }
     
@@ -100,13 +111,10 @@ describe('POST /recipes', () => {
         const recipes = await Recipe.find({})
 
         //let's check that the last item added was indeed newItem object
-        //it should contain the description "sent from Jest!"
-        expect(recipes[recipes.length-1].name).toBe("BLT PASTA SALAD")
-    })
-});
+        //it should contain the name "BLT PASTA SALAD"
+        expect(recipes[recipes.length - 1].name).toBe("BLT PASTA SALAD")
+    });
 
-// POST test for /authors route
-describe('POST /authors', () => {
     test('should add a new author to the database', async () => {
         // build a new author
         const newAuthor = {
@@ -125,7 +133,28 @@ describe('POST /authors', () => {
         const authors = await Author.find({})
 
         //let's check that the last item added was indeed newItem object
-        //it should contain the description "sent from Jest!"
+        //it should contain the name "Aysegul"
         expect(authors[authors.length-1].name).toBe("Aysegul")
+    })
+});
+
+// DELETE tests for all delete requests
+describe('DELETE requests', () => {
+    test('DELETE recipe', async () => {
+        // get recipe and parse the one you want to delete to JSON
+        const recipesAtStart = await Recipe.find({});
+        const recipeToDelete = recipesAtStart[0].toJSON();
+        // delete an item by id
+        await api
+            .delete(`/recipes/${recipeToDelete._id}`)
+            .expect(200);
+        // get all items from database again
+        const recipesAfterDelete = await Recipe.find({});
+        // check if the number of current items is one less than before
+        expect(recipesAfterDelete).toHaveLength(recipesAtStart.length - 1);
+        // get an array of all the names inside the DB
+        const recipesNames = recipesAfterDelete.map(i => i.toJSON().name)
+        // expect the name of the deleted recipe to not be there
+        expect(recipesNames).not.toContain(recipeToDelete.name);
     })
 });
