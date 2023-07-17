fecd006900120fe564ed43dc0313954806cacfb7
waheednazeer <waheed.nazeer@hotmail.com>
2023-07-17 17:58:26 +0500

    mutation testing completed for adding

--- schema/schema.js
+++ schema/schema.js
@@ -6,7 +6,8 @@ const Recipe = db.recipe;
 const { 
     GraphQLObjectType, GraphQLString, 
     GraphQLID, GraphQLInt,GraphQLSchema, 
-    GraphQLList,GraphQLNonNull 
+    GraphQLList,GraphQLNonNull,
+    GraphQLInputObjectType 
 } = graphql;
 
 //Schema defines data on the Graph like object types(book type), relation between 
@@ -24,6 +25,15 @@ const author_type = new GraphQLObjectType({
     }),
   });
 
+  const author_input_type = new GraphQLInputObjectType({
+    name: 'author_input',
+    fields: () => ({
+      name: { type: GraphQLString },
+      url: { type: GraphQLString },
+    }),
+  });
+
+
   const nutrition_type = new GraphQLObjectType({
     name: 'nutrition',
     fields: () => ({
@@ -35,6 +45,19 @@ const author_type = new GraphQLObjectType({
       sodium: { type: GraphQLString },
     }),
   });
+// for Mutatation object data type input
+const nutrition_input_type = new GraphQLInputObjectType({
+    name: 'nutrition_input',
+    fields: () => ({
+      protein: { type: GraphQLString },
+      fiber: { type: GraphQLString },
+      calories: { type: GraphQLString },
+      fat: { type: GraphQLString },
+      carbohydrates: { type: GraphQLString },
+      sodium: { type: GraphQLString },
+    }),
+  });
+
 
  
   
@@ -82,12 +105,51 @@ const RootQuery = new GraphQLObjectType({
     }
 });
  
+//Very similar to RootQuery helps user to add/update to the database.
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addRecipe: {
+            type: RecipeType,
+            args: {
+                //GraphQLNonNull make these field required
+                name: { type: new GraphQLNonNull(GraphQLString) },
+                imgUrl: { type: new GraphQLNonNull(GraphQLString) },
+                rating: { type: new GraphQLNonNull(GraphQLString) },
+                description: { type: new GraphQLNonNull(GraphQLString) },
+                cookTime: { type: new GraphQLNonNull(GraphQLString) },
+                author: { type: new GraphQLNonNull(author_input_type) },
+                equipment: { type: new GraphQLNonNull(new GraphQLList(GraphQLString)) },
+                instructions: { type: new GraphQLNonNull(new GraphQLList(GraphQLString)) },
+                ingredients: { type: new GraphQLNonNull(new GraphQLList(GraphQLString)) },
+                nutrition: { type: new GraphQLNonNull(nutrition_input_type) },
+            },
+            resolve(parent, args) {
+                let recipe = new Recipe({
+                    name: args.name,
+                    imgUrl: args.imgUrl,
+                    rating: args.rating,
+                    description: args.description,
+                    cookTime: args.cookTime,
+                    author: args.author,
+                    equipment: args.equipment,
+                    instructions: args.instructions,
+                    ingredients: args.ingredients,
+                    nutrition: args.nutrition,
+                });
+                return recipe.save();
+            }
+        }
+
+    }
+});
 
 
 //Creating a new GraphQL Schema, with options query which defines query 
 //we will allow users to use when they are making request.
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
 
 /****************************************************************************
@@ -126,23 +188,78 @@ Queries
 ***                  ***
 *** For Single Query *** 
 ***                  ***
-        {
-	        recipe (id: "64b06de5756c8eee1e1a4cd7"){
-                                id
-                                name
-                                rating
+{
+recipe (id: "64b06de5756c8eee1e1a4cd7"){
+id
+name
+rating
    
-                                author{
-                                        name
-                                        url
-                                }
-                                ingredients
-                                instructions
-                                equipment
+author{
+name
+url
+}
+ingredients
+instructions
+equipment
 
-                                nutrition{
-                                            sodium
-                                }
-	            }
-        }
+nutrition{
+sodium
+}
+
+}
+}
+
+//mutation
+mutation{
+  addRecipe(
+    name: "Microwave Corn on the Cob ", 
+    imgUrl: "https://www.budgetbytes.com/wp-content/uploads/2023/07/Microwave-Corn-on-the-Cob-V2-500x375.jpg",
+  	description: "Perfectly cooked Microwave Corn On The Cob because the last thing you want to do when it’s 156 bazillion degrees outside is turn on a grill, stove, or oven. #amiright?!?!? Cooking corn on the cob in a microwave gives you kernels that pop with sweet juiciness, and there’s no large pot of water to boil, no grill to heat and then scour clean, and no air-conditioning-killing oven to preheat.",
+    rating: "5 from 2 votes",
+    author: {name: "Monti", url: "https://www.budgetbytes.com/author/monti/"},
+    equipment: ["Microwave", "Plates", "Sticks"],
+    cookTime: "40min",
+    instructions: [
+    "Cut the cobs to fit your microwave using its inner revolving plate as your guide. Do not peel off the husks. If your corn cobs don’t have a husk and are already trimmed, just go to step 2.",
+    "Place each cob under running water and soak through. Wrap each in a wet paper towel. Place the cobs in the microwave and cook on high for 16 minutes or 4 minutes per cob. Cooking time can vary depending on the wattage of your microwave and the size of the cobs.",
+    "The cobs are done when the kernels' color has deepened and become glossy. Allow the cobs to cool until you can hold them without burning yourself. Remove the paper towel and cut off the stem end of the cobs.",
+    "Peel the husks off from the top down to the stem end. Clean off any stray strands of cornsilk. Top with 1 tablespoon of salted butter per cob and serve!"
+    ],
+    ingredients: [
+      "4 large corn cobs in their husk",
+      "4 Tbsp salted butter"
+    ],
+    nutrition: {
+      protein: "5g",
+      fiber: "3g",
+      calories: "223 Kcal",
+      fat: "13g",
+      carbohydrates: "27g",
+      sodium: "111mg"
+    }
+    
+  )
+  {
+    name
+    imgUrl
+    description
+    rating
+    author{
+      name
+      url
+    }
+    equipment
+    cookTime
+    instructions
+    nutrition{
+      protein
+      fiber
+      calories
+      fat
+      carbohydrates
+      sodium
+    }
+  }
+  
+}
 ******************************************************************************/
